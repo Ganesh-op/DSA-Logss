ca3dd951a7058dd8d9f4e7be975c810bac7f5c94
Ganesh-op <ganeshgharte001@gmail.com>
2025-09-02 18:16:20 +0530

    problem 4 solved

---
+++ Array & Hashing/1838. Frequency of the Most Frequent Element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        
+        sort(nums.begin(), nums.end());
+
+        int left = 0;
+        long long total = 0;
+        int result = 0;
+        for (int right = 0; right < nums.size(); ++right) {
+            total += nums[right];
+            while ((long long)(right - left + 1) * nums[right] > total + k) {
+                total -= nums[left];
+                left++;
+            }
+
+            result = max(result, right - left + 1);
+        }
+
+        return result;
+    }
+};
