8c39f82d0e1997e671fae3cd2e414c4d08b74afe
Ganesh-op <ganeshgharte001@gmail.com>
2025-09-10 18:24:00 +0530

    Problem 25 solved

---
+++ Array & Hashing/26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int insertIndex = 1;
+        int n = nums.size();
+
+        for(int i = 1; i < n; i++){
+            if(nums[i - 1] != nums[i]) {
+                 nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+
+    }
+};
