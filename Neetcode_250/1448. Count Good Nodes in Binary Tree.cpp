8340fdbc8dd3f614b7b7486298d52651e10215ef
Ganesh-op <ganeshgharte001@gmail.com>
2025-10-10 21:05:18 +0530

    problem 101 solveld

---
+++ Neetcode_250/1448. Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int count = 0;
+    int maxVal = INT_MIN;
+
+    void helper(TreeNode* node) {
+        if (node == nullptr) return;
+
+        int oldMax = maxVal;
+        maxVal = max(maxVal, node->val);
+
+        if (node->val == maxVal) {
+            count++;
+        }
+
+        helper(node->left);
+        helper(node->right);
+
+        // Backtrack maxVal
+        maxVal = oldMax;
+    }
+
+    int goodNodes(TreeNode* root) {
+        helper(root);
+        return count;
+    }
+};
